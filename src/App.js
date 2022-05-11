abffbad1adc2291401ee6798ec9c4ac6ea280701
Nikhil Sharma <nikhil.harsh.sharma@gmail.com>
2022-05-11 20:35:02 +0530

    feat: added data for products

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import { products } from './backend/db/products';
 
 function App() {
+  console.log(products);
   return (
     <div className='App'>
       <div>Working</div>
---
+++ src/assets/index.js
@@ -0,0 +1,83 @@
+//FICTION
+import HarryPotterAndTheCursedChild from './product-images/harry-potter-and-the-cursed-child-parts.webp';
+import MansSearchForMeaning from './product-images/man-s-search-for-meaning.webp';
+import TheArcher from './product-images/the-archer.webp';
+import TheGreatTrainJourney from './product-images/the-great-train-journey.webp';
+import TheKiteRunner from './product-images/the-kite-runner.webp';
+import ThreeThousandStitches from './product-images/three-thousand-stitches.webp';
+import TheLittlePrince from './product-images/the-little-prince.webp';
+import HomeBody from './product-images/home-body.webp';
+import WarAndPeace from './product-images/war-peace.webp';
+import LandourBazaar from './product-images/landour-bazaar.webp';
+import LoveSonnetsOfGhalib from './product-images/love-sonnets-of-ghalib.webp';
+import Siddhartha from './product-images/siddhartha.webp';
+
+//NON-FICTION
+import MyJourney from './product-images/my-journey.webp';
+import ShoeDog from './product-images/shoe-dog.webp';
+import WhyIAmAnAtheist from './product-images/why-i-am-an-atheist.webp';
+import Rajneeti from './product-images/rajneeti.webp';
+import TheDhoniTouch from './product-images/the-dhoni-touch.webp';
+import DiaryOfAYoungGirl from './product-images/the-diary-of-a-young-girl.webp';
+import IndiasStruggleForIndependence from './product-images/india-s-struggle-for-independence.webp';
+import Karma from './product-images/karma.webp';
+import SevenSecretsOfShiva from './product-images/7-secrets-of-shiva.webp';
+import RumisLittleBookOfLife from './product-images/rumi-s-little-book-of-life.webp';
+import MyGita from './product-images/my-gita.webp';
+import Death from './product-images/death.webp';
+
+//SELF-HELP
+import Ikigai from './product-images/ikigai.webp';
+import LifesAmazingSecrets from './product-images/life-s-amazing-secrets.webp';
+import StarWithWhy from './product-images/start-with-why-original.webp';
+import ThePowerOfYourSubconsiousMind from './product-images/the-power-of-your-subconscious-mind.webp';
+import ThePsycologyOfMoney from './product-images/the-psychology-of-money.webp';
+import ThinkAndGrowRich from './product-images/think-and-grow-rich.webp';
+import ThinkLikeAMonk from './product-images/think-like-a-monk.webp';
+import BeleiveInYourself from './product-images/believe-in-yourself.webp';
+import AtomicHabits from './product-images/atomic-habits.webp';
+import Mindset from './product-images/mindset.webp';
+import ChanakyaInDailyLife from './product-images/chanakya-in-daily-life.webp';
+import Attached from './product-images/attached.webp';
+
+export {
+  //FICTION
+  HarryPotterAndTheCursedChild,
+  MansSearchForMeaning,
+  TheArcher,
+  TheGreatTrainJourney,
+  TheKiteRunner,
+  ThreeThousandStitches,
+  TheLittlePrince,
+  HomeBody,
+  WarAndPeace,
+  LandourBazaar,
+  LoveSonnetsOfGhalib,
+  Siddhartha,
+  //NON-FICTION
+  MyJourney,
+  ShoeDog,
+  WhyIAmAnAtheist,
+  Rajneeti,
+  TheDhoniTouch,
+  DiaryOfAYoungGirl,
+  IndiasStruggleForIndependence,
+  Karma,
+  SevenSecretsOfShiva,
+  RumisLittleBookOfLife,
+  MyGita,
+  Death,
+  //SELF-HELP
+  Ikigai,
+  LifesAmazingSecrets,
+  StarWithWhy,
+  ThePowerOfYourSubconsiousMind,
+  ThePsycologyOfMoney,
+  ThinkAndGrowRich,
+  ThinkLikeAMonk,
+  BeleiveInYourself,
+  AtomicHabits,
+  Mindset,
+  ChanakyaInDailyLife,
+  Attached,
+};
