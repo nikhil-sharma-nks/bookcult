740ee20403634164a46af79b637fe8600b9472e8
Nikhil Sharma <nikhil.harsh.sharma@gmail.com>
2022-05-17 00:59:08 +0530

    feat: added rating and wishlist icon button

--- src/components/Products/ProductCard/ProductCard.jsx
+++ src/components/Products/ProductCard/ProductCard.jsx
@@ -10,16 +10,30 @@ const ProductCard = ({
   discountedAmount,
   discountedPercentage,
   tag,
+  rating,
 }) => {
   return (
     <div className='card vertical-card pos-rel'>
       <div className='image-container'>
         <img className='pic' src={img} alt='' />
-        {tag ? <div className='card-tag'>{tag}</div> : ''}
+        {tag ? (
+          <div className='card-tag'>
+            {tag.charAt(0).toUpperCase() + tag.slice(1)}
+          </div>
+        ) : (
+          ''
+        )}
+        <i class='fas fa-heart wishlist'></i>
       </div>
       <div className='card-info'>
         <div className='card-header'>{title}</div>
-        <div className='card-descripion mt-1'>{author}</div>
+        <div className='card-descripion mt-1'>
+          <div className='author-name'>{author}</div>
+          <div className='rating  mr-3'>
+            {rating}
+            <i class='fa-solid fa-star star-rating ml-1'></i>
+          </div>
+        </div>
         <div className='price-container my-2'>
           <div className='current-price h3'>₹{discountedPrice}</div>
           <div className='original-price h3'>₹{price}</div>
