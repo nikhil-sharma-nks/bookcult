1a1ccb1485d3bc1c741ab4aa62d3492e244ee030
Nikhil Sharma <nikhil.harsh.sharma@gmail.com>
2022-05-20 07:27:07 +0530

    feat: added toastContainer component to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
 import { Navbar } from './components';
 import RoutesContainer from './routes/RoutesContainer';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
+      <ToastContainer />
       <RoutesContainer />
     </div>
   );
