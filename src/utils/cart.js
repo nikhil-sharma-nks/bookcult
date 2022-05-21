7d96822892b362201ce9745f7ff76ec0043642c4
Nikhil Sharma <nikhil.harsh.sharma@gmail.com>
2022-05-21 06:57:06 +0530

    feat: added price addition utility function

--- src/utils/cart.js
+++ src/utils/cart.js
@@ -1,6 +1,45 @@
-const checkIfItemInCart = (title, cart) => {
-  const foundInCart = cart?.find((item) => item.title === title);
+const checkIfItemInCart = (id, cart) => {
+  const foundInCart = cart?.find((item) => item._id === id);
   return foundInCart ? true : false;
 };
 
-export { checkIfItemInCart };
+const getTotalCartItem = (cart) => {
+  const totalItems = cart.reduce((prev, current) => prev + current.qty, 0);
+  return totalItems;
+};
+
+const getTotalCartMRP = (cart) => {
+  const totalItems = cart.reduce(
+    (prev, current) => prev + current.qty * current.price,
+    0
+  );
+  return totalItems;
+};
+const getTotalDiscount = (cart) => {
+  const totalItems = cart.reduce(
+    (prev, current) => prev + current.qty * current.discountedAmount,
+    0
+  );
+  return totalItems;
+};
+const getTotalPrice = (cart) => {
+  const totalItems = cart.reduce(
+    (prev, current) => prev + current.qty * current.discountedPrice,
+    0
+  );
+  return totalItems;
+};
+const getTotalDiscountPercentage = (cart) => {
+  const price = getTotalCartMRP(cart);
+  const discount = getTotalDiscount(cart);
+  return Math.floor((discount / price) * 100);
+};
+
+export {
+  checkIfItemInCart,
+  getTotalCartItem,
+  getTotalCartMRP,
+  getTotalDiscount,
+  getTotalPrice,
+  getTotalDiscountPercentage,
+};
