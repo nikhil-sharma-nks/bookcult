d48de7d547ef27ed01a4618f2c6a4ccd79aa9d32
Nikhil Sharma <nikhil.harsh.sharma@gmail.com>
2022-07-29 12:49:07 +0530

    feat: added profile page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,3 +6,5 @@ export { Cart } from './Cart/Cart';
 export { Wishlist } from './Wishlist/Wishlist';
 export { SingleProduct } from './SingleProduct/SingleProduct';
 export { Checkout } from './Checkout/Checkout';
+export { Orders } from './Orders/Orders';
+export { Profile } from './Profile/Profile';
