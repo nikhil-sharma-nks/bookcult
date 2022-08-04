818037951d988b9c0be4c618607371ed9c877011
Nikhil Sharma <nikhil.harsh.sharma@gmail.com>
2022-08-04 14:47:41 +0530

    feat: added demo address

--- src/components/Address/Address.jsx
+++ src/components/Address/Address.jsx
@@ -1,13 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './address.scss';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { addAddress } from '../../api';
 import { updateUser } from '../../redux';
 import { makeToast } from '../../components';
-import { useNavigate } from 'react-router-dom';
 const Address = ({ isModalOpen, toggleModal }) => {
-  const authStore = useSelector((state) => state.authStore);
-  const productStore = useSelector((state) => state.productStore);
   const dispatch = useDispatch();
   const [addressInput, setAddressInput] = useState({
     name: '',
@@ -30,21 +27,65 @@ const Address = ({ isModalOpen, toggleModal }) => {
     event.preventDefault();
     try {
       const userWithUpdatedAddress = await addAddress(addressInput);
-      dispatch(updateUser(userWithUpdatedAddress));
-      makeToast('Added Address', 'success');
-      setAddressInput({
-        name: '',
-        street: '',
-        phone: '',
-        city: '',
-        state: '',
-        pincode: '',
-      });
-      toggleModal();
+      if (userWithUpdatedAddress) {
+        dispatch(updateUser(userWithUpdatedAddress));
+        makeToast('Added Address', 'success');
+        setAddressInput({
+          name: '',
+          street: '',
+          phone: '',
+          city: '',
+          state: '',
+          pincode: '',
+        });
+        toggleModal();
+      } else {
+        showErrorMessage();
+      }
     } catch (error) {
       console.log(error);
       makeToast('Error Adding Address', 'error');
     }
+  };
+
+  const demoAddress = [
+    {
+      name: 'Virat Kholi',
+      street: 'legend comeback road',
+      phone: '8877553311',
+      city: 'Delhi',
+      state: 'Delhi',
+      pincode: '110068',
+    },
+    {
+      name: 'MS Dhoni',
+      street: 'great God building',
+      phone: '777477774',
+      city: 'Ranchi',
+      state: 'Jharkhand',
+      pincode: '568833',
+    },
+    {
+      name: 'Rohit',
+      street: '200 ton road',
+      phone: '995599227',
+      city: 'Mumbai',
+      state: 'Maharashtra',
+      pincode: '110068',
+    },
+  ];
+
+  const handleAddDummyData = () => {
+    const randomDemoInput = Math.floor(Math.random() * demoAddress.length);
+    console.log('randomDemoInput', randomDemoInput);
+    setAddressInput({ ...demoAddress[randomDemoInput] });
+  };
+
+  const showErrorMessage = () => {
+    makeToast(`Action Failed, See Log For It's Reason`, 'error');
+    console.log(
+      "This function was failed because, you might have refreshed the page somewhere, since this is a frontend application which doesn't have the real backend, it uses mock backend mockbee and mirajeJs which on reloading srves entire mock backend again instead of persisting. So you might want to logout, reload and log in again with test credentials or signup again and use the features of this app without reloading"
+    );
   };
   return (
     <>
@@ -141,6 +182,15 @@ const Address = ({ isModalOpen, toggleModal }) => {
                 </button>
                 <button className='btn btn-error' onClick={() => toggleModal()}>
                   Cancel
+                </button>
+              </div>
+              <div className='btn-container mt-3'>
+                <button
+                  type='button'
+                  className='btn btn-primary-outlined mx-auto'
+                  onClick={handleAddDummyData}
+                >
+                  Add Demo Address
                 </button>
               </div>
             </form>
